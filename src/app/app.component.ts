822e0e021c320dbe6a971324d91ea442501fdaa1
hamertsabah <47854333+hamertsabah@users.noreply.github.com>
2020-09-09 23:48:31 +0300

    daily-weekly-monthly-scheduler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SchedulerEvent } from '@progress/kendo-angular-scheduler';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'daily-weekly-monthly-scheduler';
+  events: Event[] = [];
+      onNewEvent(event: Event) {
+        this.events = this.events.concat(event);
+      }
+      
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SchedulerModule } from '@progress/kendo-angular-scheduler';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { SchedulerComponent } from './scheduler/scheduler.component';
+import { HeaderComponent } from './header/header.component';
+import { EventFormComponent } from './event-form/event-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchedulerComponent,
+    HeaderComponent,
+    EventFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SchedulerModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    DateInputsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
